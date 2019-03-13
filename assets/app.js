57e57e7ba14552524aa07750f2b8cc58f171e533
phantom <phantomdesign9@gmail.com>
2019-03-12 21:52:58 -0500

    more js failed to firebase

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    //here we initialize our firebase database
     var config = {
         apiKey: "AIzaSyDiG-bLYHf9QH2vwmzABnGBqrsxyqIumkA",
         authDomain: "trainstation-dc7df.firebaseapp.com",
@@ -9,7 +10,41 @@ $(document).ready(function () {
         messagingSenderId: "819318727587"
     };
 
+    //getting real with firebase
     firebase.initializeApp(config);
-    
+
+
+    //variables for our database
     var database = firebase.database();
-}
+    var trainName;
+    var destination;
+    var firstTrain;
+    var frequency;
+
+    //click event
+    $("#submit").on("click", function () {
+        event.preventDefault();
+        database.ref().push({
+            trainName = $("#train").val().trim(),
+            destination = $("#destination").val().trim(),
+            firstTrain = $("#firsttrain").val().trim(),
+            frequency = $("#frequency").val().trim(),
+        });
+    });
+
+    database.on("child-added", function (childSnapshot) {
+
+        console.log(childSnapshot);
+
+        var dbName = childSnapshot.val().trainName;
+        var dbDestination = childSnapshot.val().destination;
+        var dbFirstTrain = childSnapshot.val().firstTrain;
+        var dbFrequency = childSnapshot.val().frequency;
+
+        //append as rows to the table
+
+        $("#schedule > tbody").append("<tr><td>" + dbName + "</td><td>" + dbDestination + "</td><td>" +
+            dbFrequency + "</td><td>" + moment(dbFirstTrain).format("hh:mm A"));
+    });
+
+});
